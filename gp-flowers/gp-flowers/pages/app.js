d65f37cc6bd5103806be168cbf58d861a78b04f5
hlsalazar <hlsalazar@espe.edu.ec>
2023-03-30 15:12:45 -0500

    relleno de codigo para evitar errores

--- gp-flowers/gp-flowers/pages/app.js
+++ gp-flowers/gp-flowers/pages/app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
-import './almacen/gestion_almacen.modulo.css'
-import './index.css'
+//import './almacen/gestion_almacen.modulo.css'
+//import './index.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- gp-flowers/gp-flowers/pages/index.tsx
+++ gp-flowers/gp-flowers/pages/index.tsx
@@ -1,8 +1,5 @@
 import Login from '../pages/login';
-import Almacen from '../pages/almacen/gestionAlmacen';
-import Proveedores from './proveedores/gestion_proveedores';
-import Reportes from './reportes/reportes';
-import Reportes_admin from './reportes/reportes_admin';
+
 
 
 
